87e4503b658a5e7fa03e9a27c897bda01524378d
Gustav Dahlberg <gdahlberg05@gmail.com>
2024-03-10 12:13:39 +0100

    skit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const slideshowWrap = document.getElementById("wrap");
 
-
+function toggle() {
+  slideshowWrap.style.display = "block";
+}
+function closeWindow() {
+  slideshowWrap.style.display = "none";
+}
 
 var slideIndex = 1;
 showDivs(slideIndex);
@@ -27,3 +33,6 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " w3-red";
 }
+
+
+
